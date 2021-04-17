8d2e5bb391044740dca5a74bf8673f698eeb4ecd
Marcus Grant <marcusfg@gmail.com>
2021-04-17 19:52:52 +0200

    Comp: First completions structure
    - Zprofile has DOTSDIRZCOMP to point to completions ./completions
    - Restructures the profile.zsh

--- profile.zsh
+++ profile.zsh
@@ -1,7 +1,23 @@
 # Zprofile
 
-export DOTSDIR="$HOME/.dots" # A useful variable to point to dotfiles dir
-export DOTSDIRZ="$DOTSDIR/zsh"
-export PATH="/opt/homebrew/bin:$PATH" # Homebrew bin
+# General variables
 export EDITOR="vim" # Default editor
 export GPG_TTY=$(tty) # GPG needs to know what shell type is in use
+
+# Dotfiles Environment Vars
+export DOTSDIR="$HOME/.dots" # A useful variable to point to dotfiles dir
+export DOTSDIRZ="$DOTSDIR/zsh"
+export DOTSDIRZCOMP="$DOTSDIRZ/completions"
+export DOTSDIRZFUNC="$DOTSDIRZ/functions"
+
+# Paths
+export PATH="/opt/homebrew/bin:$PATH" # Homebrew bin
+# fpath=+/$DOTSDIRZCOMP
+
+# # Completions
+# # GoPass
+# if [ ! -d $DOTSDIRZCOMP/_gopass ]; then
+#     if (( $+commands[gopass] )); then
+#         gopass completion zsh > $DOTSDIRZCOMP/_gopass
+#     fi
+# fi
