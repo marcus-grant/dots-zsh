7f775c6f4f6c57a5c4a0768db773822b1545c195
Marcus Grant <marcusfg@gmail.com>
2021-04-17 21:47:17 +0200

    Fix: Check prexisting homebrew path before adding

--- profile.zsh
+++ profile.zsh
@@ -1,18 +1,19 @@
 # Zprofile
 
-# General variables
+### General variables
 export EDITOR="vim" # Default editor
 export GPG_TTY=$(tty) # GPG needs to know what shell type is in use
 
-# Dotfiles Environment Vars
+### Dotfiles Environment Vars
 export DOTSDIR="$HOME/.dots" # A useful variable to point to dotfiles dir
 export DOTSDIRZ="$DOTSDIR/zsh"
 export DOTSDIRZCOMP="$DOTSDIRZ/completions"
 export DOTSDIRZFUNC="$DOTSDIRZ/functions"
 export DOTSDIRZPLUG="$DOTSDIRZ/plugins"
 
-# Paths
-export PATH="/opt/homebrew/bin:$PATH" # Homebrew bin
+### Paths
+# Homebrew package binaries
+[ "${PATH#*/opt/homebrew/bin}" ] && export PATH="/opt/homebrew/bin:$PATH"
 fpath+=$DOTSDIRZCOMP
 fpath+=$DOTSDIRZFUNC
 
