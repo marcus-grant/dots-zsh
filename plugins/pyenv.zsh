58d8ea58c0b316d94ea11199df6a4e5efd2dfb9e
Marcus Grant <marcusfg@gmail.com>
2021-04-17 22:25:51 +0200

    Refact: Only export PYENV_ROOT if pyenv present

--- plugins/pyenv.zsh
+++ plugins/pyenv.zsh
@@ -2,10 +2,10 @@
 
 # NOTE: Haven't tested in clean env, pyenv might need to explicitly add
 # ...PYENV_ROOT directory before initing. Delete this is not true after test
-# Clean up noise in home dir by moving pyenv using PYENV_ROOT
-export PYENV_ROOT=$HOME/.local/pyenv
 # If pyenv installed
 if hash pyenv 1>/dev/null 2>&1; then
+  # Clean up noise in home dir by moving pyenv using PYENV_ROOT
+  export PYENV_ROOT=$HOME/.local/pyenv
   # ..and pyenv shims aren't appearing in PATH
   if [ ! "${PATH#*$PYENV_ROOT/shims}" ]; then
     # ...then init pyenv, which will among other things put shims in path
