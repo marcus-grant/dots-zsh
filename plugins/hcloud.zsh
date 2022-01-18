cba24dbe9cd2aa17586917fc2b6aa1f0a107ce37
Marcus Grant <marcusfg@gmail.com>
2022-01-18 16:23:39 +0100

    Add hcloud-token func & export HCLOUD_TOKEN var

--- plugins/hcloud.zsh
+++ plugins/hcloud.zsh
@@ -10,3 +10,16 @@ fi
 fpath+=(~/.config/hcloud/completion/zsh)
 autoload -Uz compinit; compinit
     
+# Load hcloud into its own token variable for molecule & scripts
+# -v flag will print it
+function hcloud-token () {
+  echo "$(pass show dev/hetzner-token 2> /dev/null | tail -n 1)"
+}
+
+# Load env var for HCLOUD_TOKEN if it can be acquired
+export HCLOUD_TOKEN=$(hcloud-token)
+if [[ ${#HCLOUD_TOKEN} -lt 32 ]]; then
+  if [[ ${#HCLOUD_TOKEN} -gt 68 ]]; then
+    unset HCLOUD_TOKEN
+  fi
+fi
