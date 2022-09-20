89b394f8c1e6f725d3b3aa8d81c1be69bf80df05
Marcus Grant <marcusfg@gmail.com>
2022-09-20 18:02:54 +0200

    Add Darwin-aware LS_COLORS export
    * zsh.env
      * checks if OS_TYPE has 'darwin'
      * if darwin uses BSD based LS colors
        * cxexfxdxBxEhebAbAgAcAd
      * if not darwin uses linux LS_COLORS
        * folded lines of linux LSCOLOR
          * di=32:ln=34:so=35
          * :pi=33:ex=1;31:bd=1;34;47
          * :cd=34;41:su=1;30;41
          * :sg=1;30;46:tw=1;30;42
          * :ow=1;30;43
    * Can check on the colors using this web util below
      * https://geoff.greer.fm/lscolors/

--- env.zsh
+++ env.zsh
@@ -20,10 +20,31 @@ fpath+=$DOTSDIRZFUNC
 ### Defaults
 export EDITOR=vim
 
+# Locale
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
 # PATH
 export PATH="$PATH:$HOME/.local/bin"
 
-# Langs
+# LSCOLORS
+if [[ 'darwin' == *"$OSTYPE"* ]]; then
+    export LS_COLORS="cxexfxdxBxEhebAbAgAcAd"
+else
+    export LS_COLORS="di=32:ln=34:so=35:pi=33:ex=1;31:bd=1;34;47:cd=34;41:su=1;30;41:sg=1;30;46:tw=1;30;42:ow=1;30;43"
+fi
+
+# Programming / Runtimes
 export PATH="$PATH:$HOME/.cargo/bin"
 # The global default to use, if undefined default to system python
-export PYENV_VERSION=3.9.4
+export PYENV_VERSION=3.10.7
+
+
+# Personal/user Directories (also userdirs)
+export DIR_DOCS="$HOME/Documents"
+export DIR_NOTE="$DIR_DOCS/notes"
+export DIR_PROJ="$HOME/Projects"
+export DIR_OPS="$DIR_PROJ/ops"
+# TODO: Move infra to immediately inside root of ops & put seperate ansible/terra/shell dirs as siblings
+export DIR_INFRA="$DIR_OPS/infra"
+
